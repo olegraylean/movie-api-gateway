451a8839ad11064a5d9127dbc6dfa33bf632a171
orailean <orailean@amsoft-group.com>
2024-09-20 16:28:58 +0300

    gateway router

--- src/main/java/com/rayn_microservices/api_gateway/ApiGatewayConfiguration.java
+++ src/main/java/com/rayn_microservices/api_gateway/ApiGatewayConfiguration.java
@@ -1,2 +1,15 @@
-package com.rayn_microservices.api_gateway;public class ApiGatewayConfiguration {
+package com.rayn_microservices.api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+  @Bean
+  public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+    return builder.routes().build();
+  }
 }
